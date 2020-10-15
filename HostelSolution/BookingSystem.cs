e17bfb5dea805ee93f34943391f4f85e02e8103c
sebastianskoczen <skoczen.kontakt@gmail.com>
2020-10-15 12:58:53 +0200

    Ready solution.

---
+++ HostelSolution/BookingSystem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HostelSolution
+{
+    class BookingSystem
+    {
+        private int numberOfBeds;
+        private List<Tuple<sbyte, sbyte>> guestRequests;
+
+        public BookingSystem(int K, List<Tuple<sbyte,sbyte>> guestRequestsIn)
+        {
+            numberOfBeds = K;
+            guestRequests = guestRequestsIn;
+        }
+
+        public int CalculateMaxGuestes()
+        {
+            // Sort array of guestRequests
+            List<Tuple<sbyte, sbyte>> sortedRequests = guestRequests
+                .OrderBy(t => t.Item1)
+                .ThenBy(t => t.Item2)
+                .ToList();
+
+            // Create array of beds (in c# by default it is initialized with 0)
+            sbyte[] beds = new sbyte[numberOfBeds];
+
+            // Maximum guests counter
+            int maxGuestsCounter = 0;
+
+            // Calculate maximum possible guests - whenever a bed is available for a guest: increase the counter and change availability of the bed until the booking is finished.
+            foreach (Tuple<sbyte, sbyte> element in sortedRequests)
+            {
+                for (int i = 0; i < numberOfBeds; i++)
+                {
+                    if (element.Item1 >= beds[i])
+                    {
+                        beds[i] = element.Item2;
+                        maxGuestsCounter++;
+                        break;
+                    }
+                }
+            }
+
+            return maxGuestsCounter;
+        }
+
+    }
+}
---
+++ HostelSolution/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HostelSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the N and K (N - number of potential guests, K - number of beds)
+            string[] input = Console.ReadLine().Split(' ');
+
+            // Parse N and K variables
+            int N = Int32.Parse(input[0]);
+            int K = Int32.Parse(input[1]);
+
+            // Initialize int array storing following guest requests
+            List<Tuple<sbyte, sbyte>> guestRequests = new List<Tuple<sbyte, sbyte>>();
+
+            // Read all guest requests from the console
+            int requestCounter = 0;
+            input = Console.ReadLine().Split(' ');
+            while (input[0] != "-1" && requestCounter < N)
+            {
+                guestRequests.Add(new Tuple<sbyte, sbyte>(SByte.Parse(input[0]), SByte.Parse(input[1])));
+
+                input = Console.ReadLine().Split(' ');
+                requestCounter++;
+            }
+
+            // Init BookingSystem Instance
+            BookingSystem bs = new BookingSystem(K, guestRequests);
+
+            // Perform Calculations
+            int maxGuests = bs.CalculateMaxGuestes();
+
+            // Write the maximum guests possible to fit in given beds in the given time
+            Console.WriteLine(maxGuests);
+
+        }
+    }
+}
